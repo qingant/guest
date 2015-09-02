d7d24767a6acb633e0abd7571e90fdb76ee557d2
Ma Tao <mt@zhihu.com>
2015-09-02 13:22:44 +0800

    init

---
+++ guest/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: tao
+# @Date:   2015-09-02 12:42:37
+# @Last Modified by:   tao
+# @Last Modified time: 2015-09-02 12:42:37
---
+++ guest/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: tao
+# @Date:   2015-09-02 12:42:48
+# @Last Modified by:   tao
+# @Last Modified time: 2015-09-02 12:52:48
+
+import chaofeng
+
+print chaofeng
+
+
+def main():
+    print "hello world"
