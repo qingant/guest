cc5de3d6ecc17a3450fbb161c4c1a40bfcf34b37
Ma Tao <mt@zhihu.com>
2015-09-02 13:20:58 +0800

    init buildout env

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='guest',
+    version='1.0',
+    license='Private',
+    description='Guest bbs: modern modular telnet bbs',
+    author='matao.xjtu@gmail.com',
+    packages=find_packages('.'),
+    install_requires=['eventlet'],
+    entry_points='''
+    [console_scripts]
+    guest=guest.app:main
+    ''',
+)
